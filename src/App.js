9d71305344faf5ba360996df42eace563db2dc5d
bilimerdi <orunerdi186@gmail.com>
2023-03-18 17:38:28 +0300

    Update App.js
    Added Math library to App.js file

--- src/App.js
+++ src/App.js
@@ -2,33 +2,26 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    { title: "Car Insurance", amount: 294.37, date: new Date(23, 3, 23) },
-    { title: "New TV", amount: 100.37, date: new Date(23, 4, 23) },
-    { title: "Toilet Paper", amount: 94.37, date: new Date(23, 3, 23) },
-    { title: "New Phone", amount: 394.37, date: new Date(23, 3, 23) },
+    {
+      id: 1,
+      title: "Car Insurance",
+      amount: 294.37,
+      date: new Date(23, 3, 23),
+    },
+    { id: 2, title: "New TV", amount: 100.37, date: new Date(23, 4, 23) },
+    { id: 3, title: "Toilet Paper", amount: 94.37, date: new Date(23, 3, 23) },
+    { id: 4, title: "New Phone", amount: 394.37, date: new Date(23, 3, 23) },
+    { id: 5, title: "New Car", amount: 195.37, date: new Date(23, 3, 23) },
   ];
+
+  let expense = expenses[parseInt(Math.random() * 5)];
   return (
     <div>
       <h1>My Todosss</h1>
       <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
+        title={expense.title}
+        date={expense.date}
+        amount={expense.amount}
       ></ExpenseItem>
     </div>
   );
