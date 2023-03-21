5069f8243465b85d007c50a76f5695d0558be701
bilimerdi <orunerdi186@gmail.com>
2023-03-21 19:16:01 +0300

    Used useState for interactive in ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,36 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
+import Card from "./Card";
+import React, { useState } from "react";
+
 function ExpenseItem({ title, date, amount }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [count, setCount] = useState(0);
+
+  const clickHandler = () => {
+    setNewTitle("Updated");
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
         <div className="expense-item__price">${amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={increment}>+</button>
+        {count}
+        <button onClick={decrement}>-</button>
       </div>
-    </div>
+    </Card>
   );
 }
 
